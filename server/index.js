edc901a219a7118309ef6e9d0b2e145085db87ac
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-07-11 23:31:02 +0530

    updated the scrip

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import express from 'express';
 import axios from 'axios';
 import client from './redisClientHub.js'; 
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
--- server/redisClientHub.js
+++ server/redisClientHub.js
@@ -1,12 +1,17 @@
 // server/redisClient.js
 import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
 
 const client = createClient({
     username: 'default',
-    password: process.env.REDIS_DB_PASSWORD,
+    password: "BegG3JWI9v7nHsswwOGwxUoo1K3Xp3kJ",
     socket: {
-        host: process.env.REDIS_DB_HOST_URL,
-        port: process.env.REDIS_DB_HOST_PORT
+        host: "redis-11001.crce182.ap-south-1-1.ec2.redns.redis-cloud.com",
+        port: 11001
     }
 });
 
